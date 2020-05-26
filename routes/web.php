6a3b7e6c7900563d7f9e8f4240c3ea8cf2d859a9
Joshua Lucas <joshuamlucas91@gmail.com>
2020-05-26 12:51:08 -0400

    created route to create inn

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::POST('/inns', function () {
+    //valdate
+    //persist
+    App\Inn::create(request(
+        [
+            'host_id', 'state', 'capacity', 'catagory', 'type', 'guestspcae', 'bedrooms', 'beds', 'bathrooms', 'address', 'city', 'zip', 'emenities', 'photos', 'title', 'description', 'guest_instructions', 'start_avability', 'end_avability', 'price'
+        ]
+    ));
+    // redirect
+});
