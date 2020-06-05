de8c486b217c5ea2c0a98cd70eb26a34eb9332c0
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-05 18:44:06 -0400

    completed login compoent & fixed cors for sanctum cookies

--- resources/js/components/Auth/Login.js
+++ resources/js/components/Auth/Login.js
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { useReducer} from 'react';
+import {Redirect, useHistory} from 'react-router-dom';
 import axios from 'axios';
 
 
 const Login = () => 
 {
+    let history = useHistory();
+    // Form entry data
+    const initialState = {
+        email: "",
+        password: ""
+    };
 
+    const  reducer = (state, {field, value}) =>  {
+        return {
+            ...state,
+            [field] : value 
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleChange = e => 
+    {
+        dispatch({field :e.target.name, value: e.target.value})
+    }
+
+    const { email, password} = state
+
+    
     const handleLogin = (e) => 
     {
         e.preventDefault();
         axios.get('/sanctum/csrf-cookie').then(response => {
             axios.post('/login', {
-                email: '',
-                password: ''
+                state
             })
-            .then(response => console.log(response))
+            .then(() => {
+                history.push('/dashboard')
+            })
         });
     }
 
     return (
-        <div className="">
+        <div className="flex flex-col justify-center">
             <h1>Login</h1>
             <form onSubmit={handleLogin}>
                 <label>Email
-                    <input type="email" name="email" placeholder="email"/>
+                    <input 
+                        type="email"
+                        placeholder="email"
+                        name="email" 
+                        value={email}  
+                        onChange={handleChange}
+                     />
                 </label>
 
                 <label>Password
-                    <input type="password" name="password" placeholder="password"/>
+                    <input 
+                        type="password"
+                        placeholder="password"
+                        name="password" 
+                        value={password}  
+                        onChange={handleChange}
+                    />
                 </label>
 
                 <button type="submit">Login</button>
