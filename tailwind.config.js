12bcb91713be4d1b10f8156a9e0a9f4b38df054f
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-10 19:13:56 -0400

    add brand styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     './resources/**/*.jsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: 
+      {
+        'lightshade' : '#f7f4f1',
+        'lightaccent': '#7f9eb2',
+        'brand': '#f5b725',
+        'darkaccent': '#a5454a',
+        'darkshade': '#232c3f'
+
+      }
+    },
   },
   variants: {},
   plugins: [],
