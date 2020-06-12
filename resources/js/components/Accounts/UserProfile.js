2c1ac67b9e005d639d7e5810d4cbd7e2d885ae2f
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-12 19:17:54 -0400

    Create new user profile component

---
+++ resources/js/components/Accounts/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserProfile = () =>
+{
+    return (
+        <div>
+            <h1>This is the user profile</h1>
+        </div>
+    );
+    
+}
+
+export default UserProfile;
