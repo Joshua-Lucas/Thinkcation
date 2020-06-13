99d7ebcf0e4e6e2d5b9ee1997e510e9dc6bef0ac
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-13 13:39:11 -0400

    Move axios call and error  handle to parent el

--- resources/js/components/Accounts/AccountSettings.js
+++ resources/js/components/Accounts/AccountSettings.js
@@ -1,23 +1,56 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Switch, Route, useHistory} from 'react-router-dom';
+import Axios from 'axios';
 import UserAccount from './Useraccount.js';
 import UserProfile from './UserProfile.js';
 import UserListings from './UserListings.js';
 import PageNotFound from '../PageNotFound.js';
+import NotSignedIn from './Errors/NotSignedIn.js';
 
 
 
 const AccountSettings = () =>  {
+   
+    let history = useHistory();
+    const [user,setUser] = useState ([]);
+    const [error,setError] = useState (false);
+
+    useEffect(() => {  
+        Axios
+            .get('api/user')
+            .then(res => setUser(res.data))
+            .catch(handleErrors);
+      }, [] );
+
+    const handleLogout = () => 
+    {
+        Axios.post('/logout').then( res => {
+            history.push('/login')
+        })
+    }
+
+    const handleErrors = (err) =>  
+    {
+        if(err.response)
+        {
+            setError(true)
+        }
+    }
 
     return(
         <div>
-        <h1>This is the nav bar</h1>
+            <h1>This is the nav bar</h1>
         <Switch>
             <Route exact path="/account">
-                <UserAccount />
+                { error
+                    ?
+                    <NotSignedIn />
+                    :
+                    <UserAccount user={user} handleLogout={handleLogout}/>
+                }
             </Route>
             <Route exact path="/account/profile">
-                <UserProfile />
+                    <UserProfile />
             </Route>
             <Route exact path="/account/listings">
                 <UserListings />
