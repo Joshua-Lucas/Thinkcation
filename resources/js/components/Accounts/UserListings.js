592521371081cba87fe4503a60078f5c3cb35e0b
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-13 11:32:48 -0400

    create new UserListings componenet

---
+++ resources/js/components/Accounts/UserListings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserListings = () =>
+{
+    return (
+        <div>
+            <h1>This is the user Listings</h1>
+        </div>
+    );
+    
+}
+
+export default UserListings;
