b28f5a68d9e8d26348691a6b4414c39bcc1f25d9
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-18 19:13:54 -0400

    converted component to typescript

---
+++ resources/js/components/Inns/ShowInn.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, FunctionComponent } from "react";
+import Axios from "axios";
+
+const ShowInn: FunctionComponent = () => {
+    const [inn, setInn] = useState([]);
+
+    const apiUrl = "/api" + window.location.pathname;
+
+    useEffect(() => {
+        let url = location.href;
+        Axios.get(apiUrl).then((res) => {
+            setInn(res.data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>{inn.title}</h1>
+        </div>
+    );
+};
+
+export default ShowInn;
