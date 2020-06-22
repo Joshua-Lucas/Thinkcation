dc700aac7191ecbebf12d9e62cc092e04603e66d
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-22 16:45:37 -0400

    start migrating over to tsx

--- resources/js/components/Inns/BrowseInns.tsx
+++ resources/js/components/Inns/BrowseInns.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, FunctionComponent } from "react";
 import { Link } from "react-router-dom";
 import Axios from "axios";
 
-const BrowseInns = () => {
+const BrowseInns: FunctionComponent = () => {
     const [inns, setInns] = useState([]);
 
     useEffect(() => {
