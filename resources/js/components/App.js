787a031a115ede1ee5299bc9a450107f6a66d6ad
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-23 18:41:31 -0400

    create context for userData and provide it to app

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import UserInfoContext from "./Context/UserInforContext.js";
 import DisplayInns from "./Inns/DisplayInns.js";
 import PageNotFound from "./PageNotFound.js";
 import Login from "./Auth/Login.js";
@@ -8,30 +9,33 @@ import NavBar from "./NavBar.js";
 import AccountSettings from "./Accounts/AccountSettings.js";
 
 const App = () => {
+    const userInfoHook = useState("guest");
     return (
         <BrowserRouter>
-            <div className="">
-                <Switch>
-                    <Route exact path="/">
-                        <NavBar />
-                    </Route>
-                    <Route exact path="/login">
-                        <Login />
-                    </Route>
-                    <Route exact path="/signup">
-                        <Register />
-                    </Route>
-                    <Route path="/account">
-                        <AccountSettings />
-                    </Route>
-                    <Route path="/inns">
-                        <DisplayInns />
-                    </Route>
-                    <Route path="/*">
-                        <PageNotFound />
-                    </Route>
-                </Switch>
-            </div>
+            <UserInfoContext.Provider value={userInfoHook}>
+                <div className="">
+                    <Switch>
+                        <Route exact path="/">
+                            <NavBar />
+                        </Route>
+                        <Route exact path="/login">
+                            <Login />
+                        </Route>
+                        <Route exact path="/signup">
+                            <Register />
+                        </Route>
+                        <Route path="/account">
+                            <AccountSettings />
+                        </Route>
+                        <Route path="/inns">
+                            <DisplayInns />
+                        </Route>
+                        <Route path="/*">
+                            <PageNotFound />
+                        </Route>
+                    </Switch>
+                </div>
+            </UserInfoContext.Provider>
         </BrowserRouter>
     );
 };
