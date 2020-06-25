264ae01f07b3d3589c9d3e0179494a40473e6e84
Joshua Lucas <joshuamlucas91@gmail.com>
2020-06-25 18:24:06 -0400

    moved user info data featch call to context

--- resources/js/components/Context/UserInforContext.js
+++ resources/js/components/Context/UserInforContext.js
@@ -1,5 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import { createContext } from "react";
 
 const UserInfoContext = createContext();
+
+export const UserInfoProvider = ({ children }) => {
+    const [userInfo, setUserInfo] = useState([]);
+
+    // retriving user info
+    useEffect(() => {
+        Axios.get("/api/user")
+            .then((res) => setUserInfo(res.data))
+            .catch(handleErrors);
+    }, []);
+
+    const handleErrors = (err) => {
+        if (err.response) {
+            setUserInfo("guest");
+        }
+    };
+
+    return (
+        <UserInfoContext.Provider value={{ userInfo, setUserInfo }}>
+            {children}
+        </UserInfoContext.Provider>
+    );
+};
 
 export default UserInfoContext;
