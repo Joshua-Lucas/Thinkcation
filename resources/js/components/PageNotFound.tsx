353b34e76c3edad2d0e806a54814877650c492b9
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-01 16:01:42 -0400

    convert to TypeScript

--- resources/js/components/PageNotFound.tsx
+++ resources/js/components/PageNotFound.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Lost from "../../Lost.svg";
+import Lost from "./LostLogo";
 
-const PageNotFound = () => {
+const PageNotFound: React.FC = () => {
     return (
         <div className="flex flex-col-reverse items-center mt-12 lg:mt-20 xl:flex-row xl:justify-center xl:my-20">
             <div className="w-11/12 flex flex-col  xl:w-1/3 xl:pl-8">
                 <h1 className="text-6xl font-bold">Oops!</h1>
                 <h2 className="text-2xl pt-4">
-                    We can't seem to find the page you are looking for.
+                    We cannot seem to find the page you are looking for.
                 </h2>
                 <div className="pt-8 flex flex-col space-y-1">
                     <h3 className="text-lg font-semibold pb-2">
@@ -19,7 +19,7 @@ const PageNotFound = () => {
                 </div>
             </div>
             <div className="w-11/12 xl:w-1/3">
-                <img src={Lost} alt="404 Not Found" />
+                <Lost cssStyle="xl:px-4" />
             </div>
         </div>
     );
