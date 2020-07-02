c80d69a1f8922c60820ff1088afcd65346a99cce
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-02 11:33:03 -0400

    convert to TypeScript and update aset filepath

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const tailwindcss = require("tailwindcss");
  |
  */
 
-mix.ts("resources/js/app.js", "public/js")
+mix.ts("resources/js/app.tsx", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .options({
         processCssUrls: false,
